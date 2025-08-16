ac8cae99ff9133e3b0c2f2ae2acd9535ec0401af
Lachlan McIntyre <mcila774@student.otago.ac.nz>
2025-08-17 10:43:28 +1200

    added new dark mode and colour of the polylines

--- src/MapView.js
+++ src/MapView.js
@@ -62,7 +62,7 @@ const MapView = () => {
     <MapContainer center={foodbankPosition} zoom={13} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${MAPBOX_TOKEN}`}
-        id="mapbox/streets-v12"
+        id="mapbox/navigation-night-v1"
         tileSize={512}
         zoomOffset={-1}
         attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
@@ -84,7 +84,7 @@ const MapView = () => {
 
       {/* Polylines for Routes */}
       {routes.map((coords, idx) => (
-        <Polyline key={idx} positions={coords} color="blue" weight={3} />
+        <Polyline key={idx} positions={coords} color="#00FFFF" weight={3} />
       ))}
     </MapContainer>
   );
